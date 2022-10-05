9fbc75a902383fc4f3301e353db9aea3e7c5a447
declanqc <C20465442@mytudublin.ie>
2022-10-05 20:41:05 +0100

    added rotation

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -33,6 +33,7 @@ public class Generator : MonoBehaviour
         }
 
     }
+ 
 }
 
 
--- GE1 Examples 2022/Assets/RotateMe.cs
+++ GE1 Examples 2022/Assets/RotateMe.cs
@@ -6,16 +6,15 @@ public class RotateMe : MonoBehaviour
 {
     [Range(0, 360)]
     public float speed = 90;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-       // transform.Rotate(speed, speed, speed);
+
+     transform.Rotate(Vector3.left * Time.deltaTime * speed);
 
 
     }
