73cd0feb1f57cceb252ba6578ee65f568ee7f04f
declanqc <C20465442@mytudublin.ie>
2022-10-06 19:26:55 +0100

    added colour

--- GE1 Examples 2022/Assets/RotateMe.cs
+++ GE1 Examples 2022/Assets/RotateMe.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class RotateMe : MonoBehaviour
 {
+
+    [Header("Colors")]
+
+    public MeshRenderer meshRenderer;
+
+    [SerializeField] [Range(0f, 1f)] private float lerpTime;
+
+    [SerializeField] private Color[] lerpColors;
+
+    private int colorIndex = 0;
+
+    private float t = 0f;
+
+
     [Range(0, 360)]
     public float speed = 90;
     void Start()
@@ -14,8 +28,19 @@ public class RotateMe : MonoBehaviour
     void Update()
     {
 
-     transform.Rotate(Vector3.left * Time.deltaTime * speed);
+        transform.Rotate(Vector3.left * Time.deltaTime * speed);
 
+        transform.Rotate(speed, speed, speed);
+
+        meshRenderer.material.color = Color.Lerp(meshRenderer.material.color, lerpColors[colorIndex], lerpTime * Time.deltaTime);
+
+        t = Mathf.Lerp(t, 1f, lerpTime * Time.deltaTime);
+        if (t > .9f)
+        {
+            t = 0f;
+            colorIndex++;
+            colorIndex = (colorIndex >= lerpColors.Length) ? 0 : colorIndex;
+        }
 
     }
 }
