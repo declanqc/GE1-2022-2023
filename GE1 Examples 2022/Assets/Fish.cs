6433142e3af4711c7e22a77cde5bcfa1155e70c9
declanqc <C20465442@mytudublin.ie>
2022-10-12 15:53:25 +0100

    added rotation

--- GE1 Examples 2022/Assets/Fish.cs
+++ GE1 Examples 2022/Assets/Fish.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject head;
+    public GameObject tail;
+
+    [Range(0.0f, 5.0f)]
+    public float Frequency;
+
+    public int HeadAmplitude;
+    public int TailAmplitude;
+    public float theta;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+
+        Frequency += theta * Time.deltaTime;
+        float HeadRotation = Mathf.Sin(Frequency) * HeadAmplitude;
+        float TailRotation = Mathf.Sin(Frequency) * TailAmplitude;
+
+        head.transform.localRotation = Quaternion.AngleAxis(HeadRotation, new Vector3(0, 0, 200));
+        tail.transform.localRotation = Quaternion.AngleAxis(TailRotation, new Vector3(0, 0, 200));
+
+
+
+
     }
 }
