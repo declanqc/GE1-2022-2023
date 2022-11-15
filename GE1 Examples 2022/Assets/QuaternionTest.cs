cfee0508d4184abf641c88b611601daa81af805a
declanqc <C20465442@mytudublin.ie>
2022-11-15 19:06:26 +0000

    turret looks at player

--- GE1 Examples 2022/Assets/QuaternionTest.cs
+++ GE1 Examples 2022/Assets/QuaternionTest.cs
@@ -1,54 +1,54 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class QuaternionTest : MonoBehaviour
 {
-
+    public float turnSpeed = 10;
+    public Transform Player;
     public Transform Cube1;
     public Transform Cube2;
     public Quaternion start;
     public Quaternion end;
-    // Start is called before the first frame update
+    public bool isLooking;
     void Start()
     {
-        //  Quaternion q;
-
-         Vector3 toTarget = Cube1.position - transform.position;
-         /* toTarget.Normalize();
-          float dot = Vector3.Dot(toTarget, transform.forward);
-          float theta = Mathf.Acos(dot);
-
-          q = Quaternion.AngleAxis(theta * Mathf.Rad2Deg, Vector3.up);*/
-
-
-
-        Quaternion q = Quaternion.LookRotation(toTarget);
-       // transform.rotation = q;
-
-        Vector3 a = new Vector3(0, 0, 10);
-
-        Quaternion q1 = Quaternion.AngleAxis(90, Vector3.up);
-        Quaternion q2 = Quaternion.AngleAxis(90, Vector3.right);
-
-
-        a = q1 * a;
-
-        Debug.Log(a);
-
-        start = q;
-
-        end = Quaternion.LookRotation(Cube2.position - transform.position);
-
-       
-
-
-
     }
 
-    // Update is called once per frame
+   
+
+
     void Update()
     {
        
+    }
+
+    public void LookAtPlayer()
+    {
+         
+        
+            Vector3 dir = Player.position - transform.position;
+            Quaternion lookRot = Quaternion.LookRotation(dir);
+            lookRot.x = 0; lookRot.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Mathf.Clamp01(3.0f * Time.maximumDeltaTime));
+        
+        
+    }
+
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            isLooking = true;
+            LookAtPlayer();
+            Debug.Log("looking");
+        }
+        else
+        {
+            isLooking = false;  
+        }
     }
 }
